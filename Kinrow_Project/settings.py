a8083ba3833b66f43f6fd3f7075d612ec97795ac
Belloz <krecik704@gmail.com>
2018-05-07 22:49:06 -0400

    Added sitemaps

--- Kinrow_Project/settings.py
+++ Kinrow_Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kinrow_app',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
--- kinrow_app/urls.py
+++ kinrow_app/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from django.contrib import sitemaps
+from django.urls import path
+from .sitemaps import StaticViewSitemap
 
 app_name = "kinrow_app"
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
